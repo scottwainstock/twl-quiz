3a780e73f713f93312a7d21c02b00797a55c92f3
Scott Wainstock <ssw@rentrak.com>
2010-11-27 20:16:02 -0800

    initial flot chart

--- src/com/twlquiz/Stats.java
+++ src/com/twlquiz/Stats.java
@@ -3,11 +3,12 @@ package com.twlquiz;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class Stats extends TWLQuizUtil {
-
 	private SQLiteDatabase database;
+	WebView chart;
 	TextView highScoreTWLTwos, highScoreTWLThrees, highScoreTWLFours, highScoreTWLAll;
 
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,23 +19,33 @@ public class Stats extends TWLQuizUtil {
 		highScoreTWLThrees = (TextView)findViewById(R.id.high_score_twl_threes);
 		highScoreTWLFours  = (TextView)findViewById(R.id.high_score_twl_fours);
 		highScoreTWLAll    = (TextView)findViewById(R.id.high_score_twl_all);
+		chart = (WebView)findViewById(R.id.chart);
 
 		showStats();
+		loadChart();
+	}
+
+	private void loadChart() {
+		TWLQuizChart twlQuizChart = new TWLQuizChart(chart);
+
+		chart.getSettings().setJavaScriptEnabled(true);
+		chart.addJavascriptInterface(twlQuizChart, "twlquizchart");
+		chart.loadUrl("file:///android_asset/flot/html/chart.html");
 	}
 
 	private void showStats() {
 		database = new DatabaseHelper(this).getWritableDatabase();
 		Cursor cursor = database.rawQuery("select high from streaks where type in (?, ?, ?, ?)", LIST_TYPES);
-		
+
 		cursor.moveToPosition(0);
 		highScoreTWLAll.setText(cursor.getString(cursor.getColumnIndex("high")));
-		
+
 		cursor.moveToPosition(1);
 		highScoreTWLFours.setText(cursor.getString(cursor.getColumnIndex("high")));
 
 		cursor.moveToPosition(2);
 		highScoreTWLThrees.setText(cursor.getString(cursor.getColumnIndex("high")));
-		
+
 		cursor.moveToPosition(3);
 		highScoreTWLTwos.setText(cursor.getString(cursor.getColumnIndex("high")));
 
